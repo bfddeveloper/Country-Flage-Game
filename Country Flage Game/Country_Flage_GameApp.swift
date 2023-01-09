29015ad1b959f6af6c0a48393328513f9bdc3c96
bfddeveloper <dicksonbrody@icloud.com>
2023-01-09 09:49:41 -0600

    Initial Commit

---
+++ Country Flage Game/Country_Flage_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flage_GameApp.swift
+//  Country Flage Game
+//
+//  Created by Brody Dickson on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flage_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
